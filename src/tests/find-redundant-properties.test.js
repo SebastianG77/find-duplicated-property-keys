8ac8e93519690b1f39f4ebaa440202a5290cc3f1
SebastianG <s.gostomski@hotmail.de>
2018-09-20 00:02:13 +0200

    test(Find_Redundant_Properties): add first test case for testing a valid JSON file

--- src/tests/find-redundant-properties.test.js
+++ src/tests/find-redundant-properties.test.js
@@ -1,5 +1,14 @@
-const { test, expect } = global
+import { findRedundantProperties } from '../index.js'
+import path from 'path'
+import appRoot from 'app-root-path'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(true).toBe(true)
+const { describe, expect, it } = global
+
+let appRootPath = appRoot.toString()
+
+describe(`Validate a JSON file that does not contain any duplicates`, () => {
+  it(`returns an empty list`, () => {
+    let duplicatedProperties = findRedundantProperties(path.join(appRootPath, `./assets/test_files/valid_JSON_file.json`))
+    expect(duplicatedProperties).toHaveLength(0)
+  })
 })
