23470d3210ea5be4285bba2d713f0fc03c1fa132
SebastianG <s.gostomski@hotmail.de>
2018-09-20 01:26:39 +0200

    test(Find_Redundant_Properties): add test case for testing Property <instance>.name is duplicated

--- src/tests/find-redundant-properties.test.js
+++ src/tests/find-redundant-properties.test.js
@@ -1,6 +1,8 @@
-import { findRedundantProperties } from '../index.js'
 import path from 'path'
 import appRoot from 'app-root-path'
+
+import { PropertyKey } from '../propertykey'
+import { findRedundantProperties } from '../index'
 
 const { describe, expect, it } = global
 
@@ -12,3 +14,27 @@ describe(`Validate a JSON file that does not contain any duplicates`, () => {
     expect(duplicatedProperties).toHaveLength(0)
   })
 })
+
+describe(`Property <instance>.name is duplicated`, () => {
+  it(`returns the expected property object`, () => {
+    let duplicatedProperties = findRedundantProperties(path.join(appRootPath, `./assets/test_files/one_duplicated_string.json`))
+    comparePropertyKeyArrays(duplicatedProperties, [createPropertyKey([`<instance>`, `name`], 2)])
+  })
+})
+
+const createPropertyKey = (propertyPath, occurrence) => {
+  let propertyKey = PropertyKey(propertyPath)
+  propertyKey.occurrence = occurrence
+  return propertyKey
+}
+
+const comparePropertyKeyArrays = (result, expected) => {
+  expect(result).toHaveLength(expected.length)
+  result.forEach(resultValue => {
+    expect(expected.filter(expectedValue =>
+      resultValue.occurrence === expectedValue.occurrence &&
+      expectedValue.propertyPath.every(property =>
+        expectedValue.propertyPath[expectedValue.propertyPath.indexOf(property)]
+        === resultValue.propertyPath[expectedValue.propertyPath.indexOf(property)]))).toHaveLength(1)
+  })
+}
