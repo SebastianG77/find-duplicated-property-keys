589490a7758eed5cbade684b00fbb7f5ff4c6afa
SebastianG <s.gostomski@hotmail.de>
2019-10-13 20:27:46 +0200

    feat(alternative_spelling): rename test case file and adjust test case accordingly

--- src/tests/find-duplicated-property-keys.test.js
+++ src/tests/find-duplicated-property-keys.test.js
@@ -476,7 +476,7 @@ describe('String property <instance>.name is duplicated while option \'sensitivi
 
 describe('Validate a JSON file that does not contain any duplicates while option \'sensitivity\' is set to invalid value \'invalidSensitivity\'', () => {
   it('returns an empty list', () => {
-    expect(() => findDuplicatedPropertyKeys(readFile(path.join(ROOT_DIRECTORY, './assets/test_files/valid_JSON_file_with_invalid_sensitivity_option.json')),
+    expect(() => findDuplicatedPropertyKeys(readFile(path.join(ROOT_DIRECTORY, './assets/test_files/valid_JSON_file_with_invalid_sensitivity_option_value.json')),
       { sensitivity: 'invalidSensitivity' }))
       .toThrowError('Option \'sensitivitiy\' must be one of [\'base\', \'accent\', \'case\', \'variant\'], but contains invalid value \'invalidSensitivity\'.')
   })
